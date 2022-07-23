d013fb1715f12e40441292fffcd88d30adf424c3
umairhassan07 <71984841+umairhassan007@users.noreply.github.com>
2022-07-23 16:49:35 +0400

    fixed sidebar element positions

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -15,12 +15,6 @@
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="{{ route('about-us') }}">
-          <span class="icon-bg"><i class="mdi mdi-google-pages"></i></span>
-          <span class="menu-title">About Us</span>
-        </a>
-      </li>
-      <li class="nav-item">
         <a class="nav-link" href="{{ route('our-menu') }}">
           <span class="icon-bg"><i class="mdi mdi-book-open-page-variant"></i></span>
           <span class="menu-title">Our Menu</span>
@@ -30,12 +24,6 @@
         <a class="nav-link" href="{{ route('our-cheff') }}">
           <span class="icon-bg"><i class="mdi mdi-account-multiple-outline"></i></span>
           <span class="menu-title">Our CHEFS</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="{{ route('contact') }}">
-          <span class="icon-bg"><i class="mdi mdi-contact-mail"></i></span>
-          <span class="menu-title">Contact us</span>
         </a>
       </li>
       <li class="nav-item">
@@ -50,7 +38,18 @@
           <span class="menu-title">Meals</span>
         </a>
       </li>
-    
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('about-us') }}">
+          <span class="icon-bg"><i class="mdi mdi-google-pages"></i></span>
+          <span class="menu-title">About Us</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('contact') }}">
+          <span class="icon-bg"><i class="mdi mdi-contact-mail"></i></span>
+          <span class="menu-title">Contact us</span>
+        </a>
+      </li>
     </ul>
   </nav>
   <!-- partial -->
