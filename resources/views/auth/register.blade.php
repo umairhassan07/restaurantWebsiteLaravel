aa76d3a5661b14a1c655ac95d614b539b70d9bd0
umairhassan07 <71984841+umairhassan007@users.noreply.github.com>
2022-07-24 18:47:39 +0400

    completed user register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -74,4 +74,113 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<style>
+    .login-container{
+    margin-top: 5%;
+        margin-bottom: 5%;
+    }
+
+    .login-form-2{
+        padding: 5%;
+        background: #0062cc;
+        box-shadow: 0 5px 8px 0 rgba(0, 0, 0, 0.2), 0 9px 26px 0 rgba(0, 0, 0, 0.19);
+    }
+    .login-form-2 h3{
+        text-align: center;
+        color: #fff;
+    }
+    .login-container form{
+        padding: 10%;
+    }
+    .btnSubmit
+    {
+        width: 50%;
+        border-radius: 1rem;
+        padding: 1.5%;
+        border: none;
+        cursor: pointer;
+    }
+  
+    .login-form-2 .btnSubmit{
+        font-weight: 600;
+        color: #0062cc;
+        background-color: #fff;
+    }
+    .login-form-2 .ForgetPwd{
+        color: #fff;
+        font-weight: 600;
+        text-decoration: none;
+    }
+ 
+
+</style>
+
+<div class="container login-container">
+            <div class="row">
+                <div class="col-md-6 offset-md-3 login-form-2">
+                    <h3>Create New Account</h3>
+                    <form method="post" accept="{{ route('register') }}">
+                        @csrf
+                        <div class="form-group">
+                            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" placeholder="Your Name *" value="{{ old('name') }}" />
+                        </div>
+                        @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+
+                        <div class="form-group">
+                            <input type="text" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Your Email *" value="" />
+                        </div>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+
+                        <div class="form-group">
+                            <select name="role_as" id="" class="form-control @error('role_as') is-invalid @enderror">
+                                <option value="">Select Role</option>
+                                <option value="1">Admin</option>
+                                <option value="0">User</option>
+                            </select>
+                        </div>
+                        @error('role_as')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+
+                        <div class="form-group">
+                            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Your Password *" value="" />
+                        </div>
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                        <div class="form-group">
+                            <input type="password" name="password_confirmation" class="form-control @error('password_confirmation') is-invalid @enderror" placeholder="Password Confirm *" value="" />
+                        </div>
+                        @error('password_confirmation')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                        <div class="form-group">
+                            <input type="submit" class="btnSubmit" value="Register" />
+                            <a href="{{ route('login') }}" class="ForgetPwd" value="Login" style="float: right;">Login?</a>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+
